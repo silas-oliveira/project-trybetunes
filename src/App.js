9705dd3196817747916d327a80fcb537e6b81ea6
Silas <silas.oliveira8@gmail.com>
2021-09-15 18:28:37 -0300

    Requisito 1, criaçao das rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Favorites from './pages/Favorites';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/search" component={ Search } />
+          <Route exact path="/" component={ Login } />
+          <Route path="" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
