f2ca9282fd6fbbbc7aaf1679655b168a40d301a6
Silas <silas.oliveira8@gmail.com>
2021-09-18 14:45:05 -0300

    requisito 5, implementa um input no componente Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,13 +2,46 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
+const minArtistCaracteres = 1;
+
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artist: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      artist: event.target.value,
+    });
+  }
+
   render() {
+    const { artist } = this.state;
     return (
       <div data-testid="page-search">
         <Link to="/">Voltar à Home</Link>
         <Header />
         <p>Album</p>
+        <form action="">
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.handleChange }
+            value={ artist }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ artist.length <= minArtistCaracteres }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
